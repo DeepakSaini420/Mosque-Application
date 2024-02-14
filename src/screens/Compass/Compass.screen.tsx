1f9d6ee9a493c9fcea80316187f929249c73f123
Deepak Saini <sainideeapk40395@gmail.com>
2024-02-14 23:01:00 +0530

    Added the mosque direction in application

--- src/screens/Compass/Compass.screen.tsx
+++ src/screens/Compass/Compass.screen.tsx
@@ -1,49 +1,282 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Image } from 'react-native';
-import * as Location  from 'expo-location';
+import React, {
+  useState,
+  useEffect,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
+import PropTypes from "prop-types";
+import { Image, View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { Magnetometer } from "expo-sensors";
+import * as Location from "expo-location";
 
-const CompassApp = () => {
-  const [heading, setHeading] = useState(null);
+export const useQiblaCompass = () => {
+  const [subscription, setSubscription] = useState<any|null>(null);
+  const [magnetometer, setMagnetometer] = useState(0);
+  const [qiblad, setQiblad] = useState(0);
+  const [error, setError] = useState<string|null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const getCompassHeading = async () => {
-      try {
-        await Location.requestForegroundPermissionsAsync();
-        const subscription = await Location.watchHeadingAsync((data:any) => {
-          setHeading(data.trueHeading);
-        });
-
-        return () => {
-          subscription.remove();
-        };
-      } catch (error) {
-        console.error('Error getting compass heading:', error);
-      }
-    };
-
-    getCompassHeading();
-  }, []);
-
-  const getCardinalDirection = () => {
-    if (heading === null) {
-      return 'Calculating...';
+  const initCompass = useCallback(async () => {
+    const isAvailable = await Magnetometer.isAvailableAsync();
+    if (!isAvailable) {
+      setError("Compass is not available on this device");
+      setIsLoading(false);
+      return;
+    }
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setError("Location permission not granted");
+      setIsLoading(false);
+      return;
     }
 
-    const cardinalDirections = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-    const index = Math.round(heading / 45) % 8;
-    return cardinalDirections[index];
+    try {
+      let location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+      calculate(latitude, longitude);
+    } finally {
+      setIsLoading(false);
+      subscribe();
+    }
+  }, []);
+
+  useEffect(() => {
+    initCompass();
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const subscribe = () => {
+    Magnetometer.setUpdateInterval(100);
+    setSubscription(
+      Magnetometer.addListener((data) => {
+        setMagnetometer(angle(data));
+      })
+    );
   };
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Image 
-        source={require('../../../assets/compass.png')}
-        style={{transform:[{
-          rotate: heading ? `-${heading}deg` : '0deg'
-        }],width:380,height:380}}
-      />
-    </View>
-  );
+  const unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  const angle = (magnetometer:any) => {
+    let angle = 0;
+    if (magnetometer) {
+      let { x, y, z } = magnetometer;
+      if (Math.atan2(y, x) >= 0) {
+        angle = Math.atan2(y, x) * (180 / Math.PI);
+      } else {
+        angle = (Math.atan2(y, x) + 2 * Math.PI) * (180 / Math.PI);
+      }
+    }
+    return Math.round(angle);
+  };
+
+  const direction = (degree:number) => {
+    if (degree >= 22.5 && degree < 67.5) {
+      return "NE";
+    } else if (degree >= 67.5 && degree < 112.5) {
+      return "E";
+    } else if (degree >= 112.5 && degree < 157.5) {
+      return "SE";
+    } else if (degree >= 157.5 && degree < 202.5) {
+      return "S";
+    } else if (degree >= 202.5 && degree < 247.5) {
+      return "SW";
+    } else if (degree >= 247.5 && degree < 292.5) {
+      return "W";
+    } else if (degree >= 292.5 && degree < 337.5) {
+      return "NW";
+    } else {
+      return "N";
+    }
+  };
+
+  const degree = (magnetometer:any) => {
+    return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
+  };
+
+  const calculate = (latitude:number, longitude:number) => {
+    const PI = Math.PI;
+    let latk = (21.4225 * PI) / 180.0;
+    let longk = (39.8264 * PI) / 180.0;
+    let phi = (latitude * PI) / 180.0;
+    let lambda = (longitude * PI) / 180.0;
+    let qiblad =
+      (180.0 / PI) *
+      Math.atan2(
+        Math.sin(longk - lambda),
+        Math.cos(phi) * Math.tan(latk) -
+          Math.sin(phi) * Math.cos(longk - lambda)
+      );
+    setQiblad(qiblad);
+  };
+
+  const compassDirection = direction(degree(magnetometer));
+  const compassDegree = degree(magnetometer);
+  const compassRotate = 360 - degree(magnetometer);
+  const kabaRotate = 360 - degree(magnetometer) + qiblad;
+
+  return {
+    qiblad,
+    compassDirection,
+    compassDegree,
+    compassRotate,
+    kabaRotate,
+    error,
+    isLoading,
+    reinitCompass: initCompass,
+  };
 };
 
-export default CompassApp;
+const QiblaCompass = forwardRef(
+  (
+    { backgroundColor = "transparent", color = "#000", textStyles = {} },
+    ref
+  ) => {
+    const {
+      qiblad,
+      compassDirection,
+      compassDegree,
+      compassRotate,
+      kabaRotate,
+      error,
+      isLoading,
+      reinitCompass,
+    } = useQiblaCompass();
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          reinitCompass,
+        };
+      },
+      []
+    );
+
+    if (isLoading) {
+      return (
+        <View style={[styles.container, { backgroundColor:"transparent" }]}>
+          <ActivityIndicator size={50} color={color} />
+        </View>
+      );
+    }
+
+    return (
+      <View style={[styles.container, { backgroundColor:"transparent" }]}>
+        {error && (
+          <Text
+            style={{
+              color: "#f00",
+              fontWeight: "bold",
+              textAlign: "center",
+              paddingHorizontal: 20,
+              fontSize: 16,
+              ...textStyles,
+            }}
+          >
+            Error: {error}
+          </Text>
+        )}
+        <View style={styles.direction}>
+          <Text style={[styles.directionText, { color, ...textStyles }]}>
+            {compassDirection}
+          </Text>
+          <Text style={[styles.directionText, { color, ...textStyles }]}>
+            {compassDegree}°
+          </Text>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            height: 300,
+            position: "relative",
+          }}
+        >
+          <Image
+            source={require("../../../assets/compass.png")}
+            style={[
+              styles.image,
+              {
+                transform: [
+                  {
+                    rotate: compassRotate + "deg",
+                  },
+                ],
+              },
+            ]}
+          />
+          <View
+            style={{
+              width: 400,
+              height: 400,
+              position: "absolute",
+              alignSelf: "center",
+              transform: [
+                {
+                  rotate: `${kabaRotate}deg`,
+                },
+              ],
+              flexDirection: "row",
+              justifyContent: "center",
+              zIndex: 999,
+            }}
+          >
+            <Image
+              source={require("../../../assets/kaba.png")}
+              style={{
+                resizeMode: "center",
+                height: 100,
+                paddingBottom: 150,
+                marginTop: 20,
+                width: 60,
+                zIndex: 999,
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+);
+
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "contain",
+    alignSelf: "center",
+    position: "absolute",
+    top: 0,
+    width: 400,
+    height: 400,
+  },
+  container: {
+    backgroundColor: "#f00",
+    width:'100%',
+    height:'100%',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  direction: {
+    textAlign: "center",
+    zIndex: 300,
+  },
+  directionText: {
+    textAlign: "center",
+    fontSize: 30,
+    color: "#468773",
+  },
+  qiblaDirection: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default QiblaCompass;
